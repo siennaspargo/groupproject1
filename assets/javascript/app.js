071db064fbaff6c689809204ae482f038a7a91de
meilik <meili.ellis.tingle@gmail.com>
2018-10-18 17:09:22 -0400

    app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,65 @@
-var resp;
-var long;
-var lat;
-var date; 
-var seconds; 
+// var resp;
+// var long;
+// var lat;
+// var date; 
+// var seconds; 
+
 $(document).ready(function () {
-
-    function handleIpStackResponse(response) {
-        resp = response;
-        long = response.longitude;
-        lat = response.latitude;
-
-        var queryURL = "http://api.open-notify.org/iss-pass.json?lat=" + lat + "&lon=" + long;
-        $.ajax({
-            url: "https://floating-brushlands-91043.herokuapp.com/cors",
-            data: {
-                url: queryURL,
-                key: "6C365355271AF5033FE78FCCE1DA65A85E4193B7A5C95E92F3904ED0407F9D79"
-            },
-            method: "POST"
-        }).then(function (response) {
-            // Get reference to existing tbody element, create a new table row element
-            console.log(response);
-            date = response.response[0].risetime;
-            seconds = response.response[0].duration; 
-            console.log(date);
-            console.log(seconds);
-            date = moment.unix(date).format("MM/DD/YYYY");
-            console.log(date);
-        });
-    }
-
-    function getCoor() {
-        var queryURL = "http://api.ipstack.com/check?access_key=fdaee2744bdd8dd531effb2b6c9e23da";
+    function marsWeather(){
+        var queryURL = "https://api.maas2.jiinxt.com/";
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-            .then(handleIpStackResponse)
+            .then(function (response) {
+                console.log(response);
+            })
     }
-    getCoor();
+    
+    marsWeather();
+
+    // function handleIpStackResponse(response) {
+    //     resp = response;
+    //     long = response.longitude;
+    //     lat = response.latitude;
+
+    //     var queryURL = "http://api.open-notify.org/iss-pass.json?lat=" + lat + "&lon=" + long;
+    //     $.ajax({
+    //         url: "https://floating-brushlands-91043.herokuapp.com/cors",
+    //         data: {
+    //             url: queryURL,
+    //             key: "6C365355271AF5033FE78FCCE1DA65A85E4193B7A5C95E92F3904ED0407F9D79"
+    //         },
+    //         method: "POST"
+    //     }).then(function (response) {
+    //         // Get reference to existing tbody element, create a new table row element
+    //         // console.log(response);
+    //         // date = response.response[0].risetime;
+    //         // seconds = response.response[0].duration; 
+    //         // console.log(date);
+    //         // console.log(seconds);
+    //          for(var i = 0; i < response.response.length; i++){
+    //             var rise = response.response[i].risetime;
+    //             //console.log(rise);
+    //              rise = moment.unix(rise).format("dddd, MMMM Do, YYYY h:mm:ss A");
+    //              var dur = response.response[i].duration;
+    //              dur = Math.round(dur/60 * 100)/100; 
+    //              console.log(rise, dur); 
+    //         }
+    //         // date = moment.unix(date).format("MM/DD/YYYY");
+    //         // console.log(date);
+    //     });
+    // }
+
+    // function getCoor() {
+    //     var queryURL = "http://api.ipstack.com/check?access_key=fdaee2744bdd8dd531effb2b6c9e23da";
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     })
+    //         .then(handleIpStackResponse)
+    // }
+    // getCoor();
 
 
 });
